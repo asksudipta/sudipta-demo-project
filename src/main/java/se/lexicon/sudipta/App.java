67879247a1afba392b9962c37d4e521e898e0116
Sandip Kumar Mohapatra <sandipkumar.mohapatra@ingka.com>
2022-03-21 16:26:18 +0100

    my first java code

---
+++ src/main/java/se/lexicon/sudipta/App.java
@@ -0,0 +1,22 @@
+package se.lexicon.sudipta;
+
+
+public class App {
+    public static void main(String []args) {
+      /*
+       System.out.println( "Hello World!" );
+       Short age=100;
+       System.out.println(age);
+    */
+        int tax;
+        tax = 100;
+        System.out.println(tax);
+
+        long accountNumber;
+        accountNumber=12345678912345L;
+        System.out.println(accountNumber);
+
+
+
+    }
+}
